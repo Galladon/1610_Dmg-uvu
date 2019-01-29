2bd20cb0b383a46e3a4c85d686f716dddf866532
Benjamin <Benjaminwright42@gmail.com>
2019-01-28 17:18:33 -0700

    Created new files, and set up new stuff

---
+++ 2d_Game_1610/Assets/Scripts/Assignments/variables.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class variables : MonoBehaviour{
+// variables
+public int number;
+public float speed;
+public string name;
+    // Start is called before the first frame update
+    void Start() {
+        /*
+        This
+        Is 
+        A
+        Multy
+        Line
+        Comment
+        Example
+         */
+      number = 10; 
+      speed = 0.141f;  
+      name = "galladon";
+
+      print(name + " is "+number+" years old "+ speed);
+    }
+
+    // Update is called once per frame
+    void Update() {
+     transform.position=new Vector3( number,0,0);
+    }
+}
