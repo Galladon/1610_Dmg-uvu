07571d221cc43f0e2cc6f1232e81f492dc409d27
Benjamin <Benjaminwright42@gmail.com>
2019-01-30 17:06:02 -0700

    set up weather function for leaning variables and function

---
+++ 2d_Game_1610/Assets/Scripts/Assignments/Functions.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Functions : MonoBehaviour{
+    public string weather;
+    void Start(){
+        Weather(weather);
+    }
+   void Weather(string weatherstate){
+
+       if(weatherstate == "sunny"){
+           print("The sun is shining'");
+        
+       }
+       else if(weatherstate == "raining"){
+           print("it is damp and gloomy today");
+
+       }
+       else if(weatherstate == "snowing"){
+           print("it is freezing and slick today");
+           
+       
+       }
+         else if(weatherstate == "windy"){
+           print("it is blustrey today");
+       }
+         else if(weatherstate == "foggy"){
+           print("it is difficult to see today");
+            }
+    
+   }
+}       
+
