3f81d271c6f1d646c30b6acfc663e48bd1c53a89
Benjamin <Benjaminwright42@gmail.com>
2019-02-04 16:41:40 -0700

    set an as if to display a variable on input of my favorite pokemon type

---
+++ 2d_Game_1610/Assets/Scripts/Assignments/assignmetvariables.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class assignmetvariables : MonoBehaviour
+{
+    public string pkmtype;
+    public int preferance;
+
+    // Start is called before the first frame update
+    void Start(){
+        if(pkmtype == "electric"){
+            print("My favorite #"+preferance);
+            preferance = 2;
+            
+        }
+    }
+
+
+    
+}
