82db267592e927678b6ce18229d38529da48f147
Benjamin <Benjaminwright42@gmail.com>
2019-02-05 23:07:50 -0700

    finished else if chain with variables, and else statemt

--- 2d_Game_1610/Assets/Scripts/Assignments/assignmetvariables.cs
+++ 2d_Game_1610/Assets/Scripts/Assignments/assignmetvariables.cs
@@ -10,12 +10,33 @@ public class assignmetvariables : MonoBehaviour
     // Start is called before the first frame update
     void Start(){
         if(pkmtype == "electric"){
-            print("My favorite #"+preferance);
-            preferance = 2;
+            print("My favorite type! #"+preferance);
+            preferance = 1;
             
         }
+        else if(pkmtype == "dragon"){
+            print("Its so close to my favorite, but its #"+preferance);
+            preferance = 2;
+        }
+        else if(pkmtype == "ice"){
+            print("They're sooo cool, i love them a lot! #"+ preferance);
+            preferance = 3;
+        }
+        else if(pkmtype == "grass"){
+            print("I love a lot of these pokemon, but i prefer other types #"+preferance);
+            preferance = 4;
+        }
+        else if(pkmtype == "water"){
+            print("I like some of these pokemon, but i think they are allright #"+preferance);
+            preferance = 5;
+        }
+        else{
+            print("I'm not a huge fan of that type of pokemon");
     }
+    
 
 
     
+}
+
 }
