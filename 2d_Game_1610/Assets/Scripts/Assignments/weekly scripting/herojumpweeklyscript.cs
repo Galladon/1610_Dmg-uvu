746a9146df3d053282cfd9fdde1c8cd5da73a15b
Benjamin <Benjaminwright42@gmail.com>
2019-02-07 19:51:48 -0700

    added in a Jump to my object
    attempted to figure out how to get the gravity to turn off with a keypress, but will have to research

---
+++ 2d_Game_1610/Assets/Scripts/Assignments/weekly scripting/herojumpweeklyscript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class herojumpweeklyscript : MonoBehaviour
+{ public float jumpspeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update(){
+        if(Input.GetKeyDown(KeyCode.W)){
+             GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jumpspeed);
+        }
+       
+    }
+}
