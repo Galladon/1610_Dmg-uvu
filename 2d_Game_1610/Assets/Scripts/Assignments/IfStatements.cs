36602f7f41d158546c89355b5a5f08fdb9235f51
Benjamin <Benjaminwright42@gmail.com>
2019-02-25 17:05:46 -0700

    If statements

---
+++ 2d_Game_1610/Assets/Scripts/Assignments/IfStatements.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IfStatements : MonoBehaviour{
+
+    public string stoplight;
+    public bool isUtahn = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update(){
+        if(stoplight == "red" && isUtahn){
+
+        //code that gets run on conditions being met
+         
+            if(isUtahn){
+                print("the light is red!");
+
+
+            }   
+            else{
+                print("Thanks for stopping!");
+            }
+        }
+        else if(stoplight == "yellow"){
+            if(isUtahn){
+            print("if you're wheels make it in you're clean, so speed up!");
+            }
+
+            else {
+                print("prepare to stop!");
+            }
+    
+        }
+        else if(stoplight == "green"){
+            print(" feel free to speed more!");
+        }
+        else{
+            print("sorry for your loss, you're driving in hell");
+        }
+    }
+}
