4e105f1c5d68d15afbb04f12ee7eac0ec9fecdc6
Benjamin <Benjaminwright42@gmail.com>
2019-02-27 16:12:48 -0700

    working with as if for scripts

---
+++ 2d_Game_1610/Assets/Scripts/Assignments/weekly scripting/ifStatementsforassingments.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ifStatementsforassingments : MonoBehaviour{
+    public string fruitPreference;
+
+    // Update is called once per frame
+    void Update(){
+        if(fruitPreference = "apples"){
+            print("which type of apples?");
+        }
+        
+        else if(fruitPreference = "red apples"){
+            print("I enjoy nice and crips ones!");
+        }
+        else if(fruitPreference = "green apples"){
+            print("i really only like them in pies and carmel appels");
+        }
+        else if(fruitPreference =  )
+    }
+}
