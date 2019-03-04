20ea19eacf0e1a5c3952c786ee71eafb439325da
Benjamin <Benjaminwright42@gmail.com>
2019-03-04 16:25:41 -0700

    function work

--- 2d_Game_1610/Assets/Scripts/Assignments/Functions.cs
+++ 2d_Game_1610/Assets/Scripts/Assignments/Functions.cs
@@ -3,32 +3,48 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Functions : MonoBehaviour{
-    public string weather;
     void Start(){
-        Weather(weather);
+        print(Sum(60,9));
+        print(Multi(5,25));
     }
-   void Weather(string weatherstate){
+    public string weather;
+    //void Start(){ 
+        //Weather(weather);
+   // }
+   //void Weather(string weatherstate){
 
-       if(weatherstate == "sunny"){
-           print("The sun is shining'");
+       //if(weatherstate == "sunny"){
+           //print("The sun is shining'");
         
-       }
-       else if(weatherstate == "raining"){
-           print("it is damp and gloomy today");
+     //  }
+       //else if(weatherstate == "raining"){
+         //  print("it is damp and gloomy today");
 
-       }
-       else if(weatherstate == "snowing"){
-           print("it is freezing and slick today");
+       //}
+      // else if(weatherstate == "snowing"){
+          // print("it is freezing and slick today");
            
        
-       }
-         else if(weatherstate == "windy"){
-           print("it is blustrey today");
-       }
-         else if(weatherstate == "foggy"){
-           print("it is difficult to see today");
-            }
+       //}
+         //else if(weatherstate == "windy"){
+           //print("it is blustrey today");
+      // }
+        // else if(weatherstate == "foggy"){
+          // print("it is difficult to see today");
+            //}
     
+   
+
+   //int is a valid function type
+   int Sum( int a, int b){
+       return a + b;
+   
    }
-}       
+   int Multi( int c, int d){
+       return c * d;
+   }
+   }
+
+
+      
 
