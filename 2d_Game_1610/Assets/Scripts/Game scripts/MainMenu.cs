8404a8580d14a4c7ed7291554aa0e782c8c0a935
Benjamin <Benjaminwright42@gmail.com>
2019-03-13 17:13:26 -0600

    class
    Created main menu, with start button and exit

---
+++ 2d_Game_1610/Assets/Scripts/Game scripts/MainMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour{
+    public int levelToLoad;
+    //index for scenes
+    public void LoadLevel(){
+        SceneManager.LoadScene(levelToLoad);
+        //loads the level after the button is pressed
+
+    }
+    public void LevelExit(){
+        Application.Quit();
+    }
+    //Quits the game after you hit the button
+}
