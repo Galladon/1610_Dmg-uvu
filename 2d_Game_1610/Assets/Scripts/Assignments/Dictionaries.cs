1a49130d2278a67f11c8cd133c2ab493e4d0a4b1
Benjamin <Benjaminwright42@gmail.com>
2019-04-03 17:14:28 -0600

    Class
    Dict list array array list

---
+++ 2d_Game_1610/Assets/Scripts/Assignments/Dictionaries.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dictionaries : MonoBehaviour
+{
+    public Hashtable sprenDetails = new Hashtable();
+    // Start is called before the first frame update
+    void Start()
+    { sprenDetails.Add("Type", "Honor Spren");
+      sprenDetails.Add("Name", "Syl");
+      sprenDetails.Add("Bonded to", "Kaladin" );
+      sprenDetails.Add("Gender", "Female");
+      sprenDetails.Add("Weapon Type", "Syl Spear");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ 2d_Game_1610/Assets/Scripts/Game scripts/CameraFollow.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Heromovement hero;
+    public bool isFollowing;
+    public float xOffset;
+    public float yOffset;
+    // Start is called before the first frame update
+    void Start(){
+        hero = FindObjectOfType<Heromovement>();
+        isFollowing = true;
+
+        
+    }
+
+    // Update is called once per frame
+    void Update(){
+        if(!isFollowing){
+            transform.position = new Vector3(hero.transform.position.x + xOffset, hero.transform.position.y +yOffset, transform.position.z);
+        }
+        
+    }
+}
