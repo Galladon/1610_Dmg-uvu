66ee528efeaeaeeaeff55109683c0d1216dbf683
Benjamin <Benjaminwright42@gmail.com>
2019-04-27 23:49:26 -0600

    finished animations
    i keep forgetting to do individual pushes, but animations for both types of coin, and player are done

--- 2d_Game_1610/Assets/Scripts/Game scripts/Heromovement.cs
+++ 2d_Game_1610/Assets/Scripts/Game scripts/Heromovement.cs
@@ -6,8 +6,9 @@ public class Heromovement : MonoBehaviour {
 
 // movement varriables
 
-    public float moveSpeed;
+    public float moveSpeed ;
     public float jumpSpeed;
+    public float jumpSpeed2;
     // jump requierments
     public Transform groundCheck;
     public float groundCheckRadius;
@@ -15,7 +16,8 @@ public class Heromovement : MonoBehaviour {
     private bool doubleJump;
     
     public LayerMask whatIsGround;
-    private float moveVelocity;
+    private float moveVelocity ;
+    public Animator animator;
     
     
     // Start is called before the first frame update
@@ -30,6 +32,7 @@ public class Heromovement : MonoBehaviour {
     
     //player movement left/right
     void Update(){
+        
         if(Input.GetKey(KeyCode.D)){
             moveVelocity = moveSpeed;
             
@@ -51,23 +54,26 @@ public class Heromovement : MonoBehaviour {
         
         }
 
-        
+        animator.SetFloat("Speed", Mathf.Abs(moveVelocity));
         GetComponent<Rigidbody2D>().velocity = new Vector2(moveVelocity, GetComponent<Rigidbody2D>().velocity.y);
        
         //player jump
         if(Input.GetKeyDown(KeyCode.W) && grounded){
             Jump();
+            jumpSpeed2 = jumpSpeed;
         
         }
         //DoubleJump
         if(grounded){
             doubleJump = false;
+           
             
         }
 
         if(Input.GetKeyDown( KeyCode.W)&& !doubleJump && !grounded){
             Jump();
             doubleJump = true;
+            jumpSpeed2 = jumpSpeed;
             
         }
          moveVelocity = 0f;
@@ -78,12 +84,21 @@ public class Heromovement : MonoBehaviour {
 
         else if (GetComponent<Rigidbody2D>().velocity.x < 0)
         transform.localScale = new Vector3(-4f,4f,2f);
+
+       if(Input.GetKey (KeyCode.W)){ 
+        animator.SetBool("IsJumping", true);
+        }
+        else if(Input.GetKeyUp (KeyCode.W)){
+            animator.SetBool("IsJumping", false);
+        }
     } 
    
 void Jump(){
     
              GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jumpSpeed);
-                
+            
+             
+               
     
 }
 
