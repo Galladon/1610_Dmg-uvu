9211b9454fa9545ff9c61ef27b53bea74a8078f5
Benjamin <Benjaminwright42@gmail.com>
2019-04-28 10:25:07 -0600

    Bugfixes, level building, and finishing animations
    i know i'm pushing this, sorry Preston

---
+++ 2d_Game_1610/Assets/Scripts/Game scripts/CheckPoint.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPoint : MonoBehaviour{
+    // Start is called before the first frame update
+  public int coinValue;
+  
+
+     //piutting in colider for coin
+     void OnTriggerEnter2D(Collider2D other){
+         if(other.name == "Hero"){
+            
+
+
+                Scoremanager.AddPoints(coinValue);
+
+              Destroy(gameObject);
+         }
+     }
+}
